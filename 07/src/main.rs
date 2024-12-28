ef49515e1ff63b7ff58128d26a717430b47582da
partyfowl <14924988+partyfowl@users.noreply.github.com>
2024-12-28 13:54:06 +0000

    Day 7 part 2

--- 07/src/main.rs
+++ 07/src/main.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
 use std::collections::VecDeque;
 use std::fs;
-use std::io::{Error, ErrorKind, Result};
+use std::io::Result;
 
 fn get_param_values(
     instructions: Vec<i32>,
@@ -34,99 +34,102 @@ fn multiply(instructions: Vec<i32>, start_position: usize) -> i32 {
     return param_values[0] * param_values[1];
 }
 
-fn intcode(mut instructions: Vec<i32>, mut input: VecDeque<i32>) -> Result<Vec<i32>> {
-    let mut i: usize = 0;
-    let mut output: Vec<i32> = vec![];
+struct IntCodeMachine {
+    instructions: Vec<i32>,
+    i: usize,
+}
 
-    let len_instructions = instructions.len();
-
-    while i < len_instructions && instructions[i] != 99 {
-        let opcode_param: i32 = instructions[i];
-
-        let opcode_size: u32 = 2;
-        let opcode: i32 = opcode_param % 10_i32.pow(opcode_size);
-
-        let target: usize;
-
-        match opcode {
-            1 | 2 | 7 | 8 => target = instructions[i + 3].try_into().unwrap(),
-            3 | 4 => target = instructions[i + 1].try_into().unwrap(),
-            5 | 6 => target = usize::MAX, // value not used, but code will not compile without this
-            _ => {
-                return Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    format!("Invalid opcode: {}", opcode),
-                ))
-            }
+impl IntCodeMachine {
+    fn new(instructions: Vec<i32>) -> IntCodeMachine {
+        IntCodeMachine {
+            instructions: instructions,
+            i: 0,
         }
-
-        let interval: usize;
-
-        match opcode {
-            1 => {
-                instructions[target] = add(instructions.clone(), i);
-                interval = 4;
-            }
-            2 => {
-                instructions[target] = multiply(instructions.clone(), i);
-                interval = 4;
-            }
-            3 => {
-                instructions[target] = input.pop_front().expect("input deque empty");
-                interval = 2;
-            }
-            4 => {
-                output.push(instructions[target]);
-                interval = 2;
-            }
-            5 => {
-                let param_values = get_param_values(instructions.clone(), i, 2);
-                if param_values[0] != 0 {
-                    i = param_values[1].try_into().unwrap();
-                    interval = 0;
-                } else {
-                    interval = 3;
-                }
-            }
-            6 => {
-                let param_values = get_param_values(instructions.clone(), i, 2);
-                if param_values[0] == 0 {
-                    i = param_values[1].try_into().unwrap();
-                    interval = 0;
-                } else {
-                    interval = 3;
-                }
-            }
-            7 => {
-                let param_values = get_param_values(instructions.clone(), i, 2);
-                instructions[target] = if param_values[0] < param_values[1] {
-                    1
-                } else {
-                    0
-                };
-                interval = 4;
-            }
-            8 => {
-                let param_values = get_param_values(instructions.clone(), i, 2);
-                instructions[target] = if param_values[0] == param_values[1] {
-                    1
-                } else {
-                    0
-                };
-                interval = 4;
-            }
-            _ => {
-                return Err(Error::new(
-                    ErrorKind::InvalidInput,
-                    format!("Invalid opcode: {}", opcode),
-                ))
-            }
-        }
-
-        i += interval;
     }
 
-    return Ok(output);
+    fn proceed_until_halt(&mut self, mut input: VecDeque<i32>) -> (VecDeque<i32>, i32) {
+        let len_instructions = self.instructions.len();
+        let mut output: VecDeque<i32> = VecDeque::new();
+        while self.i < len_instructions && self.instructions[self.i] != 99 {
+            let opcode_param: i32 = self.instructions[self.i];
+
+            let opcode_size: u32 = 2;
+            let opcode: i32 = opcode_param % 10_i32.pow(opcode_size);
+
+            let target: usize;
+
+            match opcode {
+                1 | 2 | 7 | 8 => target = self.instructions[self.i + 3].try_into().unwrap(),
+                3 | 4 => target = self.instructions[self.i + 1].try_into().unwrap(),
+                5 | 6 => target = usize::MAX, // value not used, but code will not compile without this
+                _ => return (output, 1),
+            }
+
+            let interval: usize;
+
+            match opcode {
+                1 => {
+                    self.instructions[target] = add(self.instructions.clone(), self.i);
+                    interval = 4;
+                }
+                2 => {
+                    self.instructions[target] = multiply(self.instructions.clone(), self.i);
+                    interval = 4;
+                }
+                3 => {
+                    if input.is_empty() {
+                        return (output, 2);
+                    }
+                    self.instructions[target] = input.pop_front().expect("input deque empty");
+                    interval = 2;
+                }
+                4 => {
+                    output.push_back(self.instructions[target]);
+                    interval = 2;
+                }
+                5 => {
+                    let param_values = get_param_values(self.instructions.clone(), self.i, 2);
+                    if param_values[0] != 0 {
+                        self.i = param_values[1].try_into().unwrap();
+                        interval = 0;
+                    } else {
+                        interval = 3;
+                    }
+                }
+                6 => {
+                    let param_values = get_param_values(self.instructions.clone(), self.i, 2);
+                    if param_values[0] == 0 {
+                        self.i = param_values[1].try_into().unwrap();
+                        interval = 0;
+                    } else {
+                        interval = 3;
+                    }
+                }
+                7 => {
+                    let param_values = get_param_values(self.instructions.clone(), self.i, 2);
+                    self.instructions[target] = if param_values[0] < param_values[1] {
+                        1
+                    } else {
+                        0
+                    };
+                    interval = 4;
+                }
+                8 => {
+                    let param_values = get_param_values(self.instructions.clone(), self.i, 2);
+                    self.instructions[target] = if param_values[0] == param_values[1] {
+                        1
+                    } else {
+                        0
+                    };
+                    interval = 4;
+                }
+                _ => return (output, 1),
+            }
+
+            self.i += interval;
+        }
+        return (output, 0);
+    }
 }
 
 fn parse() -> Result<Vec<i32>> {
@@ -144,16 +147,52 @@ fn main() -> Result<()> {
     for combination in vec![0, 1, 2, 3, 4].iter().permutations(5) {
         let mut signal = 0;
         for a in combination {
-            let input: VecDeque<i32> = VecDeque::from([*a, signal]);
-            let output = intcode(instructions.clone(), input)?;
-            signal = *output.last().expect("output empty");
+            let mut intcode_machine = IntCodeMachine::new(instructions.clone());
+            let (output, _) = intcode_machine.proceed_until_halt(VecDeque::from([*a, signal]));
+            signal = *output.back().expect("empty output :(");
         }
 
         if signal > part_1_answer {
             part_1_answer = signal;
         }
     }
+
     println!("Part 1: {:?}", part_1_answer);
+
+    let mut highest = 0;
+
+    for combination in vec![5, 6, 7, 8, 9].iter().permutations(5) {
+        let mut intcode_machines: Vec<IntCodeMachine> = (0..5)
+            .map(|_| IntCodeMachine::new(instructions.clone()))
+            .collect();
+
+        let mut i = 0;
+        let mut output: VecDeque<i32> = VecDeque::new();
+        loop {
+            if i == 0 {
+                (output, _) =
+                    intcode_machines[i].proceed_until_halt(VecDeque::from([*combination[i], 0]))
+            } else if i < 5 {
+                output.push_front(*combination[i]);
+                (output, _) = intcode_machines[i].proceed_until_halt(output)
+            } else {
+                let return_code;
+                (output, return_code) = intcode_machines[i % 5].proceed_until_halt(output);
+                if (return_code == 0) & (i % 5 == 4) {
+                    if let Some(answer) = output.back() {
+                        if *answer > highest {
+                            highest = *answer;
+                        }
+                    }
+                    break;
+                }
+            }
+
+            i += 1
+        }
+    }
+
+    println!("Part 2: {:?}", highest);
 
     Ok(())
 }
